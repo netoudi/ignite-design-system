15a4a4c22ce1faed8785a40b3aa5c2987276be01
netoudi <netoudi@rocketmail.com>
2023-06-17 11:55:00 -0300

    tema-dark-no-storybook

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,6 +1,19 @@
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
+    backgrounds: {
+      default: 'ignite-dark',
+      values: [
+        {
+          name: 'ignite-light',
+          value: '#E1E1E6',
+        },
+        {
+          name: 'ignite-dark',
+          value: '#121214',
+        },
+      ],
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
